3f14fe84309470a4537fe4f901898ac700d72f3d
WSQS <WSQSylock@gmail.com>
2020-03-25 15:48:43 +0800

    Add files via upload
    完成了第一种检索

--- minesweeper.py
+++ minesweeper.py
@@ -6,7 +6,15 @@ c = 10
 
 #初始化地图
 map1=[0 for value in range(1,(a+2)*(b+2)+2)]#存储生成的地图
+#加入边框条件限制，对边界元素赋值为-2
+for i in range(1,a+3):
+	map1[(i-1)*(b+2)+1]=-2
+	map1[i*(b+2)]=-2
+for i in range(1,b+3):
+	map1[i]=-2
+	map1[(a+2)*(b+2)+1-i]=-2
 map2=map1[:]#确定哪些方块是可视的
+map3=map1[:]#存储已知的数字
 
 #随机生成地雷位置
 for i in range(0,c):
@@ -50,20 +58,41 @@ while map1[(i-1)*(b+2) + j]!=0:
 print(str((i-1)*(b+2) + j))
 print(map1[(i-1)*(b+2) + j])
 search1=[(i-1)*(b+2) + j]
+search2=[]
 
-#加入边框条件限制，对边界元素赋值为-2
-for i in range(1,a+3):
-	map1[(i-1)*(b+2)+1]=-2
-	map1[i*(b+2)]=-2
-for i in range(1,b+3):
-	map1[i]=-2
-	map1[(a+2)*(b+2)+1-i]=-2
 
-#生成一次点击之后的视野（已实现）将有数字的元素存入另一数组（未实现）优化数据结构（未实现）
+
+#生成一次点击之后的视野（已实现）将有数字的元素存入另一数组（未实现）
 for i in search1:
 	for k in get_around(i):
 		if map1[k] == 0 and k not in search1:
 			search1.append(k)
+		elif map1[k] != -1 and map1[k] != -2 and k not in search2 and k not in search1:
+			search2.append(k)
+			map2[k]=2
 	map2[i] = 1
+
+
+for i in search2:
+	d = 0
+	e = []
+	f = 0
+	for k in get_around(i):
+		if map2[k] == 0:
+			d=d+1
+			e.append(k)
+		elif map2[k] == 3:
+			f=f+1
+	if d == map1[i]:
+		for j in e:
+			map2[j]=3
+	if f == map1[i]:
+		for k in get_around(i):
+			if map1[k] == 0 and k not in search1:
+				search1.append(k)
+			elif map1[k] != -1 and map1[k] != -2 and k not in search2 and k not in search1:
+				search2.append(k)
+				map2[k]=2
+
 for i in range(2,a+2):
 	print(map2[(i-1)*(b+2)+2:i*(b+2)])
